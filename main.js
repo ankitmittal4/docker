30f10832c4c8cbf634212d05c32040aad61f3f18
ankitmittal4 <ankitmittal0814@gmail.com>
2025-04-05 11:01:18 +0530

    Create node server

---
+++ main.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.get('/', (req, res) => {
+    return res.json({ message: 'I am Nodejs Container, Bye' });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on PORT: ${PORT}`);
+});
