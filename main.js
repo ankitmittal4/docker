b425619e579ba83b7e0cb1a0868bdd2c9348ad8b
Ankit <ankitmittal0814@gmail.com>
2026-02-02 18:03:45 +0530

    Update version

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     // return res.send('I am Nodejs Container, Bye');
-    return res.json({ message: 'I am Nodejs Container, Bye !!! V4' });
+    return res.json({ message: 'I am Nodejs Container, Bye !!! V6' });
 });
 
 app.listen(PORT, () => {
